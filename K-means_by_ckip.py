8c09e035743502da1c7fbccba5060e821f6e27fb
Hua-cin <lazyso1989@gmail.com>
2020-07-06 14:48:40 +0800

    modify-no complete

--- K-means_by_ckip.py
+++ K-means_by_ckip.py
@@ -70,23 +70,22 @@ for j in article:
 # for x in article_lst_dict:
 #     print(x,article_lst_dict[x])
 
-# # def getList(dict):
-# #     list = []
-# #     for key in dict.keys():
-# #         list.append(key)
-# #
-# #     return list
-# #
-# # def comb_key(dict_list, k):
-# #     list = []
-# #     for j in dict_list:
-# #         for i in j:
-# #             if i in list:
-# #                 pass
-# #             elif i in getList(j)[:k]:
-# #                 list.append(i)
-# #     return list
-# #
+# def getList(dict):
+#     list = []
+#     for key in dict.keys():
+#         list.append(key)
+#
+#     return list
+#
+# def comb_key(list, dict_list, k):
+#
+#     for j in dict_list:
+#         if j in list:
+#             pass
+#         elif j in getList(j)[:k]:
+#             list.append(i)
+#     return list
+
 
 # define combine key function for mutl article
 def comb_key(list, dict_list):
@@ -106,13 +105,18 @@ def comb_key(list, dict_list):
 columns = ["article_name"]
 content=[]
 for y in article_lst_dict:
-    comb_key(columns, article_lst_dict[y] )
+    comb_key(columns, article_lst_dict[y])
     content.append(article_lst_dict[y])
 
 print(columns)
 print(content)
 
 dd = pd.DataFrame(data=content,columns=columns)
+
+for i in range(dd.shape[0]):
+    dd.iloc[i,1:] = dd.iloc[i,1:] / dd["香蕉"][i]
+# test.iloc[0,1:] = test.iloc[0,1:]/test["A"][0]
+
 dd = dd.set_index("article_name")
 dd = dd.sort_index(axis=0)
 dd = dd.fillna(value=0)
@@ -122,7 +126,7 @@ print(dd)
 from sklearn import cluster
 
 # KMeans 演算法
-kmeans_fit = cluster.KMeans(n_clusters = 4).fit(dd)
+kmeans_fit = cluster.KMeans(n_clusters = 3).fit(dd)
 
 # 印出分群結果
 cluster_labels = kmeans_fit.labels_
