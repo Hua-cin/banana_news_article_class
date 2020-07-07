0540964e43a5dcc18c5dfd0d10b379ed425f3393
Hua-cin <lazyso1989@gmail.com>
2020-07-08 03:38:38 +0800

    modify-...

--- K-means_after_tfidf_by_jieba.py
+++ K-means_after_tfidf_by_jieba.py
@@ -17,17 +17,6 @@ import re
 
 def main():
 
-
-    # insert stopword list
-    stopword_path = r'./01_ref_data/stopword.txt'
-    stopword_list = []
-    with open(stopword_path, 'r', encoding='utf-8') as f_stop:
-        for temp in f_stop.readlines():
-            stopword_list.append(temp.replace('\n', ''))
-    print(stopword_list)
-
-
-
     # insert sample
     # insert no related source data--------------------
     sample_path = r'./02_data_warehouse/sample'
@@ -42,10 +31,25 @@ def main():
                 sample_text += line
     # print(sample_text)
 
-    sample_word_cut_dict = func_jieba(sample_text, stopword_list)
-    print("sample")
-    print(sample_word_cut_dict)
+    sample_word_cut_dict = func_jieba(sample_text)
+    # print("sample")
+    # print(sample_word_cut_dict)
 
+    # insert related source data----------------------
+    related_path = r'./02_data_warehouse/related'
+    related_file_list = os.listdir(related_path)
+
+    related_text = ''
+    for each_article in related_file_list:
+        article_path = related_path + "/" + each_article
+        with open(article_path, 'r', encoding='utf-8') as f:
+            temp = f.read()
+            for line in temp:
+                related_text += line
+    # print(no_related_text)
+
+    related_word_cut_dict = func_jieba(related_text)
+    # print(related_word_cut_dict)
 
 
     # insert no related source data--------------------
@@ -61,25 +65,8 @@ def main():
                 no_related_text += line
     # print(no_related_text)
 
-    no_related_word_cut_dict = func_jieba(no_related_text, stopword_list)
-    print(no_related_word_cut_dict)
-
-
-    # insert related source data----------------------
-    related_path = r'./02_data_warehouse/related'
-    related_file_list = os.listdir(related_path)
-
-    related_text = ''
-    for each_article in related_file_list:
-        article_path = related_path + "/" + each_article
-        with open(article_path, 'r', encoding='utf-8') as f:
-            temp = f.read()
-            for line in temp:
-                related_text += line
-    # print(no_related_text)
-
-    related_word_cut_dict = func_jieba(related_text, stopword_list)
-    print(related_word_cut_dict)
+    no_related_word_cut_dict = func_jieba(no_related_text)
+    # print(no_related_word_cut_dict)
 
 
     countlist = [related_word_cut_dict, no_related_word_cut_dict]
@@ -100,20 +87,17 @@ def main():
     base = {}
     j = 0
     for i, count in enumerate(countlist):
-        print("Top words in document {}".format(i+1))
+        # print("Top words in document {}".format(i+1))
         scores = {word: tfidf(word, count, countlist) for word in count}
         sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
         cd ={}
-        for word, score in sorted_words[1:100]:
-            print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+        for word, score in sorted_words[0:150]:
+            # print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
             cd[word] = round(score, 5)
 
         base[j]=cd
         j += 1
     # print(base)
-
-
-
 
 
     base[2] = sample_word_cut_dict
@@ -133,26 +117,48 @@ def main():
     h = h.fillna(0)
 
     h.iloc[:2, :] = h.iloc[:2, :] * 2000
-    h.iloc[2:, :] = h.iloc[2:, :] * 1
-    # h.iloc[:2, :10] = h.iloc[:2, :10] /1.8
+    m_5 = ['價格', '颱風','產量','市場','農民', '採收']
+    if h.loc[2,"香蕉"] >= 4:
+        for i in m_5:
+            if h.loc[2,i] > 0:
+                h.loc[2,i] = h.loc[0,i]
 
+
+    late = h.iloc[:,1:]
+
+    # print(late)
 
     # KMeans 演算法
-    kmeans_fit = cluster.KMeans(n_clusters=2).fit(h)
+    kmeans_fit = cluster.KMeans(n_clusters=2).fit(late)
 
     # 印出分群結果
     cluster_labels = kmeans_fit.labels_
-    print("分群結果：")
-    print(cluster_labels)
+    # print("分群結果：")
+    # print(cluster_labels)
+
+    if cluster_labels[0]==cluster_labels[1]:
+        print("無相關")
+    elif cluster_labels[1] == cluster_labels[2]:
+        print("無相關")
+    else:
+        print("相關")
 
     # 使用pandas 將資料轉為csv檔
-    h.to_csv('./dd.csv', index=0, encoding="utf_8_sig")
+    late.to_csv('./dd.csv', index=0, encoding="utf_8_sig")
 
-def func_jieba(text, stopword_list):
+def func_jieba(text):
     '''
     :param text:
     :return: word count dict
     '''
+
+    # insert stopword list
+    stopword_path = r'./01_ref_data/stopword.txt'
+    stopword_list = []
+    with open(stopword_path, 'r', encoding='utf-8') as f_stop:
+        for temp in f_stop.readlines():
+            stopword_list.append(temp.replace('\n', ''))
+    # print(stopword_list)
 
     jieba.load_userdict(r'./01_ref_data/mydict.txt')
     s = jieba.cut(text)
@@ -163,7 +169,7 @@ def func_jieba(text, stopword_list):
         else:
             jieba_word_count[i] = 1
 
-    jieba_word = [(k, jieba_word_count[k]) for k in jieba_word_count if (len(k) > 1) and (k not in stopword_list)]
+    jieba_word = [(k, jieba_word_count[k]) for k in jieba_word_count if (len(k) > 1) and (k not in stopword_list) and not re.match(r'[0-9a-zA-Z]+',k)]
     jieba_word.sort(key=lambda item: item[1], reverse=True)
 
     jieba_dict = {}
--- test.py
+++ test.py
@@ -7,8 +7,8 @@ columns = ["A","B","C"]
 test = pd.DataFrame(data=data, columns=columns)
 test = test.fillna(0)
 print(test)
-test.iloc[2:,:] = test.iloc[2:,:]*10
-print(test)
+# test.loc[2,"A"] = test.loc[2,"A"]*10
+print(test.iloc[:,1:])
 
 # for i in range(test.shape[0]):
 #     if test["A"][i] == 0:
